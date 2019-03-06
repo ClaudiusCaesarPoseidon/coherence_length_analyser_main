291499cf3cb161c197843af3a50e76e70af64e31
claudiuscaesarposeidon <haarmeyer@out-ev.de>
2019-03-06 16:30:03 +0100

    dsf

--- structure.py
+++ structure.py
@@ -1,12 +1,18 @@
 import os
 
 def list_files(startpath):
+    lst = []
     for root, dirs, files in os.walk(startpath):
         level = root.replace(startpath, '').count(os.sep)
         indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
+        lst.append('{}{}/'.format(indent, os.path.basename(root)))
+#        print('{}{}/'.format(indent, os.path.basename(root)))
         subindent = ' ' * 4 * (level + 1)
         for f in files:
-            print('{}{}'.format(subindent, f))
+            lst.append('{}{}'.format(subindent, f))
+#            print('{}{}'.format(subindent, f))
+    string = '\n'.join(lst)
+    with open("structure.txt", "w") as file:
+        file.write(string)
 
 list_files(r"C:\Users\Haarmeyer\Desktop\git\coherence_length_analyser\coherence_length_analyser")
